454043ac24122115372bb7a3dd1cab2bef08bf2d
hokyun <wjdghrbs2468@naver.com>
2024-01-14 20:17:38 +0900

    fix:allowed host update

--- side_project/settings.py
+++ side_project/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+ALLOWED_HOSTS = [
+    ".ap-northeast-2.compute.amazonaws.com",
+    "127.0.0.1",
+]
